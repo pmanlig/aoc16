d9382199275960bab8400f793e777f098014eccd
pmanlig <patrik@manlig.org>
2020-12-05 21:32:28 +0100

    Day 6

--- src/Solvers/S6.js
+++ src/Solvers/S6.js
@@ -3,7 +3,16 @@ import Solver from './Solver';
 
 export class S6a extends Solver {
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		input = input.split('\n');
+		let chars = input[0].split('').map(c => { return {}; });
+		input.forEach(l => {
+			for (let i = 0; i < l.length; i++) {
+				if (chars[i][l[i]] === undefined) { chars[i][l[i]] = 1; } else { chars[i][l[i]]++; }
+			}
+		});
+		let msg = chars.map(c => Object.keys(c).map(k => { return { ch: k, n: c[k] } }).sort((a, b) => b.n - a.n)[0].ch).join('');
+		let msg2 = chars.map(c => Object.keys(c).map(k => { return { ch: k, n: c[k] } }).sort((a, b) => a.n - b.n)[0].ch).join('');
+		this.setState({ solution: `Message(1): ${msg}\nMessage(2): ${msg2}` });
 	}
 }
 
