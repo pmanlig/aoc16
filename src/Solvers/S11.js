d84b27e34683aaa81fcf4fc1a22fdcc5a9d67a2c
pmanlig <patrik@manlig.org>
2021-01-05 16:20:20 +0100

    Day 11

--- src/Solvers/S11.js
+++ src/Solvers/S11.js
@@ -1,18 +1,27 @@
 import "./S11.css";
 import React from 'react';
 import Solver from './Solver';
+import * as Util from '../util';
 
-const direction = {
-	up: -1,
-	dn: 1
-}
+let num_substances = 5;
 
 const abbreviations = {
 	polonium: "Po",
 	thulium: "Th",
 	promethium: "Pr",
 	ruthenium: "Ru",
-	cobalt: "Co"
+	cobalt: "Co",
+	elerium: "El",
+	dilithium: "Di"
+}
+
+function HMS(time) {
+	let pad = (n, l) => { n = n.toString(); while (n.length < l) { n = '0' + n; } return n; }
+	let hrs = Math.floor(time / (1000 * 60 * 60));
+	let mins = Math.floor(time / (1000 * 60)) % 60;
+	let sec = Math.floor(time / 1000) % 60;
+	let ms = time % 1000;
+	return `${pad(hrs, 2)}:${pad(mins, 2)}:${pad(sec, 2)}.${pad(ms, 3)}`;
 }
 
 const floors = {
@@ -32,9 +41,9 @@ class Substance {
 }
 
 class Item {
-	constructor(s, t) {
-		this.substance = s;
-		this.type = t;
+	constructor(substance, type) {
+		this.substance = substance;
+		this.type = type;
 	}
 
 	column() {
@@ -42,54 +51,72 @@ class Item {
 	}
 }
 
-class Move {
-	constructor(dir, cargo) {
-		this.direction = dir;
-		this.cargo = cargo;
-	}
-}
-
 class State {
-	constructor(i, prev) {
-		this.data = i;
-		this.prev = prev;
+	constructor(data, prev) {
+		this.data = data;
+		this.prev = prev === undefined ? null : prev;
+		this.moves = this.prev === null ? 0 : prev.moves + 1;
+		this.wt = this.moves + this.distance();
 	}
 
-	toList() {
-		let i = this.data;
-		let list = [];
-		while (list.length < 11) {
-			list.push(i & 3);
-			i = i >> 2;
-		}
-		return list;
+	getItem(id) {
+		return (this.data / (4 ** id)) & 3;
 	}
 
 	distance() {
-		let i = this.data;
-		let r = 0;
-		while (i > 0) {
-			r += i & 3;
-			i = i >> 2;
+		let d = 0, x = this.data;
+		while (x > 0) {
+			d += x & 3;
+			x /= 4;
 		}
-		return r;
+		return d;
 	}
 
-	static fromList(list, prev) {
-		let i = 0;
-		while (list.length > 0) {
-			i = (i << 2) + list.pop();
+	move(dir, items) {
+		let n = this.data;
+		n += dir; // elevator
+		items.forEach(i => n += dir * (4 ** i));
+		return new State(n, this);
+	}
+
+	key() {
+		let elevator = this.data & 3, items = [], x = Math.floor(this.data / 4);
+		for (let i = 0; i < num_substances * 2; i++) {
+			items.push(x & 3);
+			x = Math.floor(x / 4);
 		}
-		return new State(i, prev);
+		let chips = items.slice(0, num_substances);
+		let gens = items.slice(num_substances);
+		let pairs = [];
+		while (chips.length > 0) { pairs.push(gens.shift() * 4 + chips.shift()); }
+		pairs.sort(Util.numericSortAsc);
+		for (let m = 4; pairs.length > 0; m *= 16) {
+			elevator += pairs.shift() * m;
+		}
+		return elevator;
+	}
+
+	valid() {
+		let items = [], x = Math.floor(this.data / 4); // remove elevator
+		for (let i = 0; i < num_substances * 2; i++) {
+			items.push(x & 3);
+			x = Math.floor(x / 4);
+		}
+		let chips = items.slice(0, num_substances);
+		let gens = items.slice(num_substances);
+		for (let c = 0; c < num_substances; c++) {
+			if (chips[c] !== gens[c] && gens.some(g => g === chips[c])) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	static fromContents(contents, elevator, prev) {
 		for (let f = 0; f < contents.length; f++) {
 			for (let i = 0; i < contents[f].length; i++) {
 				let item = contents[f][i];
-				let sh = (4 * item.substance.id + (item.type === "microchip" ? -2 : 0));
-				// console.log(`f: ${f}, id: ${item.substance.id}, shift: ${sh}, f << sh: ${f << sh}`);
-				elevator += f << sh;
+				elevator += f * (4 ** (item.substance.id + (item.type === "microchip" ? 0 : num_substances)));
 			}
 		}
 		return new State(elevator, prev);
@@ -119,8 +146,7 @@ export class S11a extends Solver {
 					subst = new Substance(p[1], this.colors.shift(), substances.length + 1);
 					substances.push(subst);
 				}
-				let item = new Item(subst, p[2]);
-				contents[floor].push(item);
+				contents[floor].push(new Item(subst, p[2]));
 			});
 		}
 	}
@@ -144,73 +170,100 @@ export class S11a extends Solver {
 		contents[elevator + move.direction] = contents[elevator + move.direction].concat(move.cargo);
 	}
 
-	move(moves, contents, substances) {
-		if (moves.length === 0) { return; }
-		let m = moves.shift();
-		let p = m.match(/^(up|dn) (\d)(\w)\s?(\d)?(\w)?$/);
-		let elev = this.state.elevator;
-		let cargo = [];
-		cargo.push(contents[elev].find(i => i.substance.id === parseInt(p[2], 10) && i.type === (p[3] === "g" ? "generator" : "microchip")));
-		if (p[4]) {
-			cargo.push(contents[elev].find(i => i.substance.id === parseInt(p[4], 10) && i.type === (p[5] === "g" ? "generator" : "microchip")));
-		}
-		contents[elev] = contents[elev].filter(x => !cargo.includes(x));
-		elev += p[1] === "up" ? -1 : 1;
-		contents[elev] = contents[elev].concat(cargo);
-		if (!this.validate(contents)) { this.setState({ error: true }); }
-		this.setState({ contents: contents, moves: this.state.moves + 1, elevator: elev });
-		setTimeout(() => this.move(moves, contents, substances), 500);
-	}
-
-	generateMoves(contents, elevator) {
-		let moves = [];
-		let floor = contents[elevator];
-		if (elevator < 3) {
-			// generate down moves
-			for (let i = 0; i < floor.length; i++) {
-				moves.push(new Move(direction.dn, [floor[i]]));
+	select1to2(items) {
+		let p = [];
+		for (let i = 0; i < items.length - 1; i++) {
+			for (let j = i + 1; j < items.length; j++) {
+				p.push([items[i], items[j]]);
 			}
 		}
-		if (elevator > 0) {
-			// generator up moves
-		}
+		return items.map(i => [i]).concat(p);
+	}
+
+	generateMoves(state, seen) {
+		let e = state.getItem(0), moves = [];
+		let items = [];
+		for (let i = 1; i <= num_substances * 2; i++) { if (state.getItem(i) === e) { items.push(i); } }
+		let permutations = this.select1to2(items);
+		if (e > 0) { moves = moves.concat(permutations.map(p => state.move(-1, p)).filter(s => s.valid())); } // move up
+		if (e < 3 && items.some(i => i > e)) { moves = moves.concat(permutations.map(p => state.move(1, p)).filter(s => s.valid())); } // move down
+		moves = moves.filter(m => !seen.has(m.key()));
+		moves.forEach(m => { seen.add(m.key()); });
 		return moves;
 	}
 
-	solve(input) {
-		let contents = [];
-		let substances = [];
-		let moves = [];
-		input = input.split('\n').forEach(s => this.parse(s, contents, substances));
-		console.log(State.fromContents(contents, 3));
-		this.setState({ contents: contents, substances: substances, moves: 0, elevator: 3 });
-		setTimeout(() => this.move(moves, contents, substances), 500);
-	}
-
-	renderContents() {
-		if (!this.state.contents) { return null; }
-		let contents = this.state.contents;
-		let items = [];
-		for (let y = 0; y < contents.length; y++) {
-			contents[y].forEach(i => {
-				items.push(<div key={i.substance.name + i.type} className="cell item" style={{ gridColumn: i.column(), gridRow: y + 1, backgroundColor: i.type === "microchip" ? "white" : i.substance.color }}>{i.substance.abbreviation}</div>);
-			});
+	findCandidate(moves) {
+		for (let i = 0; true; i++) {
+			if (moves[i] !== undefined && moves[i].length > 0) {
+				return moves[i].pop();
+			}
 		}
-		return items;
 	}
 
-	render() {
-		return <div>
+	merge(moves, newMoves) {
+		while (newMoves.length > 0) {
+			let n = newMoves[0];
+			if (moves[n.wt] === undefined) { moves[n.wt] = []; }
+			moves[n.wt] = moves[n.wt].concat(newMoves.filter(m => m.wt === n.wt));
+			newMoves = newMoves.filter(m => m.wt !== n.wt);
+		}
+	}
+
+	explore2(moves, seen, n, start) {
+		let s;
+		for (let i = 0; i++ < 1000; n++) {
+			s = this.findCandidate(moves);
+			if (s.data === 0) {
+				this.setState({ state: s, steps: n, time: Date.now() - start, seen: seen });
+				return;
+			}
+			this.merge(moves, this.generateMoves(s, seen));
+		}
+		this.setState({ state: s, steps: n });
+		setTimeout(() => this.explore2(moves, seen, n, start), 1);
+	}
+
+	explore(initial) {
+		let moves = [], seen = new Set([initial.key()]);
+		moves[initial.wt] = [initial];
+		setTimeout(() => this.explore2(moves, seen, 0, Date.now()), 1);
+	}
+
+	solve(input) {
+		let contents = [], substances = [], part2 = true;
+		input.split('\n').forEach(s => this.parse(s, contents, substances));
+		if (part2) {
+			let elerium = new Substance("elerium", this.colors.shift(), 6);
+			let dilithium = new Substance("dilithium", this.colors.shift(), 7);
+			substances.push(elerium);
+			substances.push(dilithium);
+			contents[3].push(new Item(elerium, "microchip"));
+			contents[3].push(new Item(elerium, "generator"));
+			contents[3].push(new Item(dilithium, "microchip"));
+			contents[3].push(new Item(dilithium, "generator"));
+		}
+		num_substances = substances.length;
+		let initial = State.fromContents(contents, 3);
+		this.setState({ substances: substances, state: initial });
+		this.explore(initial);
+	}
+
+	customRender() {
+		let { state, substances, steps, time, seen } = this.state;
+		return <div style={{ padding: "5px" }}>
 			<div className="grid">
 				<div className="cell" style={{ gridColumn: 1, gridRow: 4 }}>1:</div>
 				<div className="cell" style={{ gridColumn: 1, gridRow: 3 }}>2:</div>
 				<div className="cell" style={{ gridColumn: 1, gridRow: 2 }}>3:</div>
 				<div className="cell" style={{ gridColumn: 1, gridRow: 1 }}>4:</div>
-				{this.renderContents()}
-				{this.state.elevator && <div className="cell item" style={{ gridColumn: 2, gridRow: 1 + this.state.elevator }}>E</div>}
+				<div className="cell item" style={{ gridColumn: 2, gridRow: 1 + state.getItem(0) }}>E</div>
+				{substances && substances.map(s => <div key={s.name + "chip"} className="cell item" style={{ gridColumn: s.id * 2 + 1, gridRow: 1 + state.getItem(s.id), backgroundColor: "white" }}>{s.abbreviation}</div>)}
+				{substances && substances.map(s => <div key={s.name + "gen"} className="cell item" style={{ gridColumn: s.id * 2 + 2, gridRow: 1 + state.getItem(s.id + num_substances), backgroundColor: s.color }}>{s.abbreviation}</div>)}
 			</div>
-			<p>Moves: {this.state.moves}</p>
-			{this.state.error && <p>Error! Invalid state!</p>}
+			<p>Moves: {state.moves}</p>
+			<p>Moves considered: {steps}</p>
+			{time && <p>Time: {HMS(time)}</p>}
+			{seen && <p># of seen states: {seen.size}</p>}
 		</div>;
 	}
 }
